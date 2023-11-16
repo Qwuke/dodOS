b2998d848e5aaa485f9b3cbd9cd9bbdfe4e97a35
Tristan Mortimer <tristan.e.mortimer@gmail.com>
2023-11-16 14:58:57 -0500

    Add some fun colors

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,15 @@ static HELLO_WORLD: &[u8] = b"dodOS is NOT extinct!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
-
+    let mut color_increment: u8 = 0;
     for (i, &byte) in HELLO_WORLD.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb + color_increment;
+            color_increment = color_increment.wrapping_add(64);
         }
     }
-    
+
     loop {}
 }
 
